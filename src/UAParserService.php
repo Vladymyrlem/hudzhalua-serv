efdfcd3a15865224bfa608e419e77877b43860f1
Vladymyr <vovangud@gmail.com>
2022-11-04 21:57:41 +0200

    Init hudzhalua serv

---
+++ src/UAParserService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Hudzhal\UserAgent;
+
+use donatj\UserAgent\UserAgentParser;
+
+class UAParserService implements UserAgentServiceInterface
+{
+    protected $userAgent;
+
+    public function __construct()
+    {
+        $this->userAgent = new UserAgentParser();
+        $this->userAgent = $this->userAgent->parse();
+    }
+
+    public function browser(): ?string
+    {
+        return $this->userAgent->browser();
+    }
+
+    public function os(): ?string
+    {
+        return $this->userAgent->platform();
+    }
+}
